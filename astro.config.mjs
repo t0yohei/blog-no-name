77597543353d9e9f1d1888e3bc55003b141de6d0
t0yohei <inuneko690121@gmail.co>
2023-01-03 17:10:52 +0900

    add netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  output: "server",
+  adapter: netlify()
 });
